0ec01b2cb1bf4b8f92e5f8d757d5c52e660583ad
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-11 22:22:50 +0900

    feat: Add health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
+
+#[cfg(test)]
+mod test {
+    use actix_web::{body::MessageBody, test};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_health_check() {
+        let app =
+            test::init_service(App::new().route("/health_check", web::get().to(health_check)))
+                .await;
+        let req = test::TestRequest::get().uri("/health_check").to_request();
+        let res = test::call_service(&app, req).await;
+
+        let status = res.status();
+        let body = res.into_body().try_into_bytes().unwrap();
+
+        assert_eq!(status, 200, "status isn't 200.");
+        assert!(body.is_empty(), "body is not empty.");
+    }
 }
