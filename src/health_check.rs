02048bf6de2a1acc98c5832f689212311287477b
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-13 20:57:28 +0900

    test: Refactor health_check unit test for better readability and maintainability

--- src/health_check.rs
+++ src/health_check.rs
@@ -6,22 +6,21 @@ pub async fn health_check() -> impl Responder {
 
 #[cfg(test)]
 mod test {
-    use actix_web::{body::MessageBody, test, web, App};
+    use actix_web::{body::MessageBody, test};
 
     use super::*;
 
     #[actix_web::test]
     async fn test_health_check() {
-        let app =
-            test::init_service(App::new().route("/health_check", web::get().to(health_check)))
-                .await;
-        let req = test::TestRequest::get().uri("/health_check").to_request();
-        let res = test::call_service(&app, req).await;
+        let req = test::TestRequest::default().to_http_request();
+        let response = health_check().await;
+        let response = response.respond_to(&req);
 
-        let status = res.status();
-        let body = res.into_body().try_into_bytes().unwrap();
-
-        assert_eq!(status, 200, "status isn't 200.");
-        assert!(body.is_empty(), "body is not empty.");
+        assert!(response.status().is_success());
+        let body = response.into_body().try_into_bytes();
+        match body {
+            Ok(body) => assert!(body.is_empty()),
+            Err(e) => panic!("Failed to convert body into bytes: {:?}", e),
+        }
     }
 }
