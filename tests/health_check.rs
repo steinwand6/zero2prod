1f84e2877fedb5b5f48c034ed45b459bb153751e
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-13 23:01:05 +0900

    feat: Add database integration and update CI for testing
    - Added database integration for storing subscriber data
    - Updated tests to check if data is persisted in the database (currently failing)
    - Modified GitHub Actions CI to include PostgreSQL setup for integration tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,8 @@
 use std::net::TcpListener;
 
 use reqwest::Client;
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -20,10 +22,16 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_from_data() {
     let address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
+
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = Client::new();
 
     let test_case = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-
     let response = client
         .post(format!("{address}/subscriptions"))
         .header("Content-Type", "application/x-www-form-urlencoded")
@@ -33,6 +41,13 @@ async fn subscribe_returns_a_200_for_valid_from_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!("ursula_le_guin@gmail.com", saved.email);
+    assert_eq!("le guin", saved.name);
 }
 
 #[tokio::test]
