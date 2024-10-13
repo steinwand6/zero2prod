4491dc157753dbbd69c267c11c0cf573cea577df
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-14 02:26:17 +0900

    chore: Add Podman configuration for PostgreSQL setup in init_db.sh

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -20,39 +20,42 @@ APP_USER_PWD="${APP_USER_PWD:=secret}"
 APP_DB_NAME="${APP_DB_NAME:=newsletter}"
 
 # Launch postgres using Podman
-CONTAINER_NAME="postgres"
-podman run \
-  --env POSTGRES_USER=${SUPERUSER} \
-  --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
-  --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
-  --health-interval=1s \
-  --health-timeout=5s \
-  --health-retries=5 \
-  --publish "${DB_PORT}":5432 \
-  --detach \
-  --name "${CONTAINER_NAME}" \
-  postgres -N 1000
-  # ^ Increased maximum number of connections for testing purposes
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  CONTAINER_NAME="postgres"
+  podman run \
+    --env POSTGRES_USER=${SUPERUSER} \
+    --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+    --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+    --health-interval=1s \
+    --health-timeout=5s \
+    --health-retries=5 \
+    --publish "${DB_PORT}":5432 \
+    --detach \
+    --name "${CONTAINER_NAME}" \
+    postgres -N 1000
+    # ^ Increased maximum number of connections for testing purposes
 
-# Wait for Postgres to be ready to accept connections
-until [ \
-  "$(podman inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
-  "healthy" \
-]; do     
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1 
-done
+  # Wait for Postgres to be ready to accept connections
+  until [ \
+    "$(podman inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+    "healthy" \
+  ]; do     
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1 
+  done
 
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+  >&2 echo "Postgres is up and running on port ${DB_PORT}!"
 
 
-# Create the application user
-CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
-podman exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
+  # Create the application user
+  CREATE_QUERY="CREATE USER ${APP_USER} WITH PASSWORD '${APP_USER_PWD}';"
+  podman exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${CREATE_QUERY}"
 
-# Grant create db privileges to the app user
-GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
-podman exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+  # Grant create db privileges to the app user
+  GRANT_QUERY="ALTER USER ${APP_USER} CREATEDB;"
+  podman exec -it "${CONTAINER_NAME}" psql -U "${SUPERUSER}" -c "${GRANT_QUERY}"
+fi
 
 DATABASE_URL=postgres://${APP_USER}:${APP_USER_PWD}@localhost:${DB_PORT}/${APP_DB_NAME}
 export DATABASE_URL
