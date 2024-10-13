1b63d02ce02bfcb49e8a41d6a2de9e7964c46b28
Tomoya Ishigaki <steinwand.tomo@gmail.com>
2024-10-13 20:36:54 +0900

    test: Add unit test for subscription with valid input

--- src/subscriptions.rs
+++ src/subscriptions.rs
@@ -10,3 +10,22 @@ pub struct FormData {
 pub async fn subscribe(_form: Form<FormData>) -> impl Responder {
     HttpResponse::Ok().finish()
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use actix_web::test;
+
+    #[actix_web::test]
+    async fn subscribe_with_valid_input() {
+        let form_data = FormData {
+            email: "test@example.com".to_string(),
+            name: "test".to_string(),
+        };
+        let req = test::TestRequest::default().to_http_request();
+        let input = Form(form_data);
+        let response = subscribe(input).await;
+        let response = response.respond_to(&req);
+        assert!(response.status().is_success());
+    }
+}
